f9aa167a33005366a4a92a2fc8d1cc46981f20af
Taylor Doodchenko <taylordoodchenko@Taylors-MacBook-Pro.local>
2018-07-22 16:42:07 -0400

    java: code for randomizing word array and displaying word as '_' is done... cannot load js script in html???

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,22 +1,22 @@
-// javascript for word guessing game 
+// Select random word from array and display characters of word with '_' 
 
+var WordsArray = ["apple", "cherry", "blueberry", "pecan", "pizza", "peach"] 
+var RandoWord = Math.floor(Math.random() * WordsArray.length)
+var wordX = WordsArray[RandoWord]
+var WordChars = wordX.split("")
+var Underscore = []
+for (var i =0; i < WordChars.length; i++) {
+    Underscore.push("_ ")
+}
 
+console.log (RandoWord)
+console.log (Underscore)
+var stringChars = Underscore.join("")
+document.querySelector(".Word-ShowChar").innerHTML = stringChars
 
-//create array of words 
-// variables
+document.querySelector(".guesses").innerHTML = Underscore
+var guesses=10
 
-    // code
-
-    var WordsArray = ["apple", "cherry", "blueberry", "pecan", "pizza", "peach"] 
-    var RandoWord = Math.floor(Math.random() * WordsArray.length)
-    var Word = WordsArray[RandoWord]
-    var WordChars = word.split("")
-    var Underscore = []
-    for (var i=0; i<WordChars.length; i++) {
-        Underscore.push("_ ")
-    }
-
-    document.getElementById("display-word").innerHTML = (Underscore);
-    var 
-    
-}}
+// Show the amount of Guesses remaining
+// Listen for player typing letters
+// Add letters in place of '_'. Otherwise Remove 1 guess and document the wrong guesses
