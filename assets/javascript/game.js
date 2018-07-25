b4833b99826a7875f0a28f8d2dc3db979e5e6926
Taylor Doodchenko <taylordoodchenko@Taylors-MacBook-Pro.local>
2018-07-24 22:23:57 -0400

    now have coded so that the correct letters that are typed will be shown in place of the corresponding underscore

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -15,30 +15,35 @@ for (var i =0; i < WordChars.length; i++) {
 // strChars is a variable that joins together all of the Underscores (each representative of a character in the chosen word)
 var StrChars = Underscore.join("")
 document.getElementById("output").innerHTML = StrChars
-// Guesses is a variable for the amount of guesses that you have to complete the word without the game ending
+
 var Guesses = 8
 document.getElementById("guesses").innerHTML= Guesses
 
-// Below is where I will code the interactive aspect of the word-guessing-game
 
-// firstly I need to have the browser recognize when someone has typed a letter and create a variable for that guess
 document.addEventListener("keyup", function(event) {
-    var x = event.charCode || event.keyCode;
-    var guess = String.fromCharCode(x);   
-    console.log(x)
-   
- // secondly I will structure am IF statement that will say that if the guess is correct it will replace the underscore in the 'strChars' string   
-    
-    console.log(guess)
+    var guess = event.key; 
+       
     console.log(WordChars)
-    if (guess == wrdCmp) { 
-    alert("correct") }
-   
+    console.log(guess)
+     
+    // if (wordX.toLowerCase().includes(guess)) { 
+       
+    // alert("Good guess dude!")
+    
+    // console.log(StrChars)
 
 
-
-})
+    // }
+   for (var i = 0; i < WordChars.length; i++) {
+      if (guess === WordChars[i]) {
+        Underscore[i] = guess;
+        document.getElementById("output").innerHTML = Underscore.join("")
+      }
+    }
+    
+  })
 }
+
 
 
 
